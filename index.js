1191200a9d70fa10acbf5443a22a3e6a868f9793
Andreazzini, Davide <Davide.Andreazzini@newday.co.uk>
2018-09-21 13:20:37 +0100

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
+app.use((req, res, next)=>{
+  console.log(`HTTP request - ${req.hostname} - ${req.ip} - ${req.url}`)
+  next()
+})
 // serving static files
 app.use(express.static("public"))
 
